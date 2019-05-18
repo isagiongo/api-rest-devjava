423db7ac43c6e5855372a4db34026151063cec48
Isa Giongo <isagiongo@gmail.com>
2019-05-18 16:22:31 -0300

    Inicia api rest

---
+++ src/main/java/com/isagiongo/apirestdevjava/ApiRestDevjavaApplication.java
@@ -0,0 +1,13 @@
+package com.isagiongo.apirestdevjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestDevjavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestDevjavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isagiongo/apirestdevjava/ApiRestDevjavaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.isagiongo.apirestdevjava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiRestDevjavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
